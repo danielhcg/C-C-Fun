a9b8b5085441ffc5f5b7bd353fabb42a24f3079a
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-03-13 05:57:02 +0000

    Added a for loop

--- .ccls-cache/@home@runner@CC-Fun/main.cpp
+++ .ccls-cache/@home@runner@CC-Fun/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
-  std::cout << "This is for a test commit";
+  // LOOP CONSTRUCTS
+
+  // FOR LOOP
+  for (int i = 0; i < 5; i++) {
+    cout << i + 1 << endl;
+  }
+  // WHILE LOOP
+  // DO WILE LOOP
 }
