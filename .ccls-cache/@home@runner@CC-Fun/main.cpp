2a6d29cbc0e4ee6a1d2d7bd4c92c79f7f58235f1
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-03-13 21:12:27 +0000

    created a do while loop

--- .ccls-cache/@home@runner@CC-Fun/main.cpp
+++ .ccls-cache/@home@runner@CC-Fun/main.cpp
@@ -5,14 +5,24 @@ int main() {
   // LOOP CONSTRUCTS
 
   // FOR LOOP
+  cout << "Created using a for loop      : ";
   for (int i = 0; i < 5; i++) {
-    cout << i + 1 << endl;
+    cout << i + 1 << " ";
   }
+  cout << "\n";
   // WHILE LOOP
+  cout << "Created using a while loop    : ";
   int i = 1;
   while (i <= 5) {
-    cout << i << endl;
+    cout << i << " ";
     i++;
   }
+  cout << "\n";
   // DO WILE LOOP
+  cout << "Created using a do while loop : ";
+  int j = 1;
+  do {
+    cout << j << " ";
+    j++;
+  } while (j <= 5);
 }
